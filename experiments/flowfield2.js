b72e12d924e3660b792d1ff74a98012befc24fb1
hamda-02 <saha23hm@student.ju.se>
2025-09-18 22:08:37 +0200

    #week 3 added

--- experiments/flowfield2.js
+++ experiments/flowfield2.js
@@ -1,25 +1,35 @@
 //Citaion: https://www.youtube.com/watch?v=sZBfLgfsvSk
 
 let particles = [];
-const num = 600;
+const num = 200;
 
-const noiseScale = 0.01; //zoom in and make it less grainy
+const noiseScale = 0.006; //zoom in and make it less grainy
 
-let CurrentStroke = [44, 113, 114];
+// let CurrentStroke = [
+//   [34, 139, 34], // Forest Green
+//   [107, 142, 35], // Olive Green
+//   [144, 238, 144], // Light Green (leaves in sunlight)
+// ];
 
 function setup() {
   createCanvas(innerWidth, innerHeight);
   for (let i = 0; i < num; i++) {
     particles.push(createVector(random(width), random(height)));
   }
-  background(0);
+  background(255);
 }
 
 function draw() {
   // background(255, 6);
+  //changing color automatically
+  if (frameCount % (60 * 2) === 0) {
+    // CurrentStroke = random(CurrentStroke);
+    stroke(0);
+    noiseSeed(millis());
+  }
 
-  stroke(CurrentStroke[0], CurrentStroke[1], CurrentStroke[2], 50);
-  strokeWeight(1);
+  // stroke(CurrentStroke[0], CurrentStroke[1], CurrentStroke[2], 50);
+  strokeWeight(random(1, 4));
   for (let i = 0; i < num; i++) {
     let p = particles[i];
     point(p.x, p.y);
@@ -32,13 +42,13 @@ function draw() {
     let a = TAU * n;
 
     // spped + here i'm converting angle into x & y
-    let speed = map(noise(p.x * 0.05, p.y * 0.05), 0, 1, 0.5, 3);
+    let speed = map(noise(p.x * 0.05, p.y * 0.05), 0, 1, 0.5, 6);
     p.x += cos(a) * speed;
     p.y += sin(a) * speed;
 
+    p.y += sin(frameCount * 0.01 + i) * 0.3;
     //Chatgpt suggestion Line 37
-    line(prevX, prevY, p.x, p.y); // draw the line as particle moves
-
+    line(prevX, prevY, p.x, p.y);
     if (!onScreen(p)) {
       //when ps leave the canvas reset
       p.x = random(width);
@@ -47,11 +57,6 @@ function draw() {
   }
 }
 
-function mousePressed() {
-  noiseSeed(millis());
-  //random color on click
-  CurrentStroke = [random(255), random(255), random(255)];
-}
 function onScreen(v) {
   //check ps on screen
   return v.x >= 0 && v.x <= width && v.y >= 0 && v.y <= height;
