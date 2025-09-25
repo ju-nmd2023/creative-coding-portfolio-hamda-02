2ea6e5fe4801849d3dd0f3ee6e1422d51750e35c
hamda-02 <saha23hm@student.ju.se>
2025-09-25 21:19:30 +0200

    week 4- tone.js not working!!!

---
+++ experiments/ball.js
@@ -0,0 +1,55 @@
+let pos;
+let ballFill = color(255);
+// let y = 0;
+let synth;
+let xSpeed = random([8, -8]);
+let ySpeed = random([8, -8]);
+
+window.addEventListener("load", () => {
+  synth = new Tone.MonoSynth().toDestination();
+});
+
+function setup() {
+  createCanvas(600, 400);
+  pos = createVector(200, 0);
+  ballFill = color(255);
+  xSpeed = random([8, -8]);
+  ySpeed = random([8, -8]);
+}
+
+window.addEventListener("click", () => {
+  Tone.start();
+});
+
+function draw() {
+  background(0);
+  // strokeWeight(2);
+  // stroke(255);
+  fill(ballFill);
+  ellipse(pos.x, pos.y, 60, 60);
+
+  // if (pos.y > height) {
+  //   ySpeed *= -1;
+  //   ballFill = color(random(255), random(255), random(255));
+  // }
+
+  if (pos.y < 0 || pos.y > height) {
+    ySpeed *= -1;
+    ballFill = color(random(255), random(255), random(255));
+    synth.triggerAttackRelease("C4", "8n");
+  }
+  if (pos.x < 0 || pos.x > width) {
+    xSpeed *= -1;
+    ballFill = color(random(255), random(255), random(255));
+    synth.triggerAttackRelease("A3", "5n");
+  }
+  // if (pos.x > width) {
+  //   xSpeed *= -1;
+  //   ballFill = color(random(255), random(255), random(255));
+  // }
+
+  pos.y = pos.y + ySpeed;
+  pos.x = pos.x + xSpeed;
+}
+
+//Citation: What Make Art https://www.youtube.com/watch?v=y14SpHKL1gg
---
+++ experiments/flowfieldsquars.js
@@ -0,0 +1,81 @@
+//Citaion: https://www.youtube.com/watch?v=sZBfLgfsvSk
+let particles = [];
+
+let colorPalette = [
+  //De Stijl Piet Mondrian color palette
+  [216, 219, 215], // light gray
+  [231, 5, 3],     // red
+  [3, 0, 173],     // blue
+  [253, 222, 6],   // yellow
+];
+
+
+let c;
+const num = 10;
+
+const noiseScale = 0.0007; //zoom in and make it less grainy
+
+// let CurrentStroke = [
+//   [34, 139, 34], // Forest Green
+//   [107, 142, 35], // Olive Green
+//   [144, 238, 144], // Light Green (leaves in sunlight)
+// ];
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  for (let i = 0; i < num; i++) {
+    particles.push(createVector(random(width), random(height)));
+  }
+  c = random(colorPalette);
+
+  background(255);
+}
+
+function draw() {
+  //changing color automatically
+  if (frameCount % (60 * 2) === 0) {
+    // CurrentStroke = random(CurrentStroke);
+    c = random(colorPalette); //new color change
+
+    noiseSeed(millis());
+  }
+  stroke(0);
+  strokeWeight(2);
+  fill(c[0], c[1], c[2], c[3]);
+  for (let i = 0; i < num; i++) {
+    let p = particles[i];
+    point(p.x, p.y);
+    // ellipse(p.x, p.y, 0.04 + noise(p.x, p.y) * 5);
+
+    //Chatgpt suggestion Line 25-27
+    let prevX = p.x;
+    let prevY = p.y;
+    let n = noise(p.x * noiseScale, p.y * noiseScale);
+    let a = TAU * n;
+
+    if (i % 2 === 0) {
+      a += PI;
+    }
+
+    // spped + here i'm converting angle into x & y
+    let speed = map(noise(p.x * 0.05, p.y * 1), 0, 1, 0.5, 9);
+    p.x += cos(a) * speed;
+    p.y += sin(a) * speed;
+
+    p.y += sin(frameCount * 0.09 + i) * 9;
+    //Chatgpt suggestion Line 37
+    rect(prevX, prevY, 60);
+    // line(prevX, prevY, p.x, p.y);
+
+    if (!onScreen(p)) {
+      //when ps leave the canvas reset
+      p.x = random(width);
+      p.y = random(height);
+    }
+  }
+}
+
+function onScreen(v) {
+  //check ps on screen
+  return v.x >= 0 && v.x <= width && v.y >= 0 && v.y <= height;
+}
